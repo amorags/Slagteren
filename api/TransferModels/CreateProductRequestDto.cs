3276df98fa5dfca3e34b60d3f97ac2faa68603d3
amorags <115067025+amorags@users.noreply.github.com>
2023-12-18 18:34:59 +0100

    Product Validation

--- api/TransferModels/CreateProductRequestDto.cs
+++ api/TransferModels/CreateProductRequestDto.cs
@@ -4,15 +4,39 @@ namespace api.TransferModels;
 
 public class CreateProductRequestDto
 {
+    [Required(ErrorMessage = "Product Number is required.")]
+    [RegularExpression(@"^\d{6}$", ErrorMessage = "Product Number must be a 6-digit number.")]
     public int ProductNumber { get; set; }
-    [MinLength(5)]
+
+    [Required(ErrorMessage = "Product Name is required.")]
+    [MinLength(5, ErrorMessage = "Product Name must be at least 5 characters long.")]
     public string ProductName { get; set; }
+
+    [Required(ErrorMessage = "Price is required.")]
+    [Range(0.01, double.MaxValue, ErrorMessage = "Price must be greater than 0.")]
     public double PricePrKilo { get; set; }
+
+    [Required(ErrorMessage = "Product Type is required.")]
+    [Range(1, int.MaxValue, ErrorMessage = "Please select a valid product type.")]
     public int ProductType { get; set; }
+
+    [Required(ErrorMessage = "Country of Birth is required.")]
+    [StringLength(50, ErrorMessage = "Country of Birth should be at most 50 characters.")]
     public string CountryOfBirth { get; set; }
+
+    [Required(ErrorMessage = "Production Country is required.")]
+    [StringLength(50, ErrorMessage = "Production Country should be at most 50 characters.")]
     public string ProductionCountry { get; set; }
-    [MaxLength(500)]
+
+    [Required(ErrorMessage = "Description is required.")]
+    [MaxLength(500, ErrorMessage = "Description should be at most 500 characters.")]
     public string Description { get; set; }
+
+    [Required(ErrorMessage = "Image URL is required.")]
+    [Url(ErrorMessage = "Invalid URL format for Image.")]
     public string ImgUrl { get; set; }
+
+    [Required(ErrorMessage = "Minimum Expiry Date is required.")]
+    [Range(1, int.MaxValue, ErrorMessage = "Minimum Expiry Date should be a positive number.")]
     public int MinExpDate { get; set; }
 }
